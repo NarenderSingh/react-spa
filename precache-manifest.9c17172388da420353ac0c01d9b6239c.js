dffbc8be637bb8bcbc12c9c0588c9b8f6b0e5157
NarenderSingh <naren.delhi13@gmail.com>
2020-02-15 17:08:47 +0530

    Updates

---
+++ precache-manifest.9c17172388da420353ac0c01d9b6239c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c8f3fbb0b07a60dcdbbcb54b96a5a7d9",
+    "url": "/react-spa/index.html"
+  },
+  {
+    "revision": "2c60f2b0ca35e0291510",
+    "url": "/react-spa/static/css/main.e323c81c.chunk.css"
+  },
+  {
+    "revision": "8084e87aa6564f0a74f4",
+    "url": "/react-spa/static/js/2.f0ccfdb1.chunk.js"
+  },
+  {
+    "revision": "608ba5ece62cd2c14649ef57eba241d6",
+    "url": "/react-spa/static/js/2.f0ccfdb1.chunk.js.LICENSE"
+  },
+  {
+    "revision": "2c60f2b0ca35e0291510",
+    "url": "/react-spa/static/js/main.8f35d75f.chunk.js"
+  },
+  {
+    "revision": "a099d96b4ae58be93417",
+    "url": "/react-spa/static/js/runtime-main.7d0fcea9.js"
+  }
+]);
